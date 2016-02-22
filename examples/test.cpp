ee700b4c611c8dac42ba1d032f4a3082e0156bec
collinsc <chris.collins@is4s.com>
2016-02-22 10:07:32 -0600

    Fixed logs in spectracom_example

--- examples/test.cpp
+++ examples/test.cpp
@@ -4,12 +4,14 @@
 int main(int argc, char **argv)
 {
   if(argc < 3) {
-      std::cerr << "Usage: spectracom-test <ip address> <port number>" << std::endl;
+      std::cerr << "Usage: spectracom_example <ip address> <port number>" << std::endl;
       return 0;
   }
 
   spectracom::Spectracom * device = new spectracom::Spectracom;
   
+  device->log_.connect(boost::bind(&logutils::printLogToStdOut, _1, _2));
+                       
   std::string ip(argv[1]);
   int port=5025;
   istringstream(argv[2]) >> port;
@@ -18,7 +20,7 @@ int main(int argc, char **argv)
   if (!device->connect(ip,port)) {return -1;}
 
   // loop forever
-    double power;
+//    double power;
   
   while(1) {
       device->queryError();
